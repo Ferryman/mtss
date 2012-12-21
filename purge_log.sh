67a38e996ec7a94545f8cd2f8f36b95492b7bfde
Ferryman <tongyancn+github@gmail.com>
2012-12-21 23:06:45 +0800

    add admin_emails file

--- purge_log.sh
+++ purge_log.sh
@@ -4,9 +4,10 @@
 LogDir=/tmp/scripts/logs
 DataDir=/tmp/scripts/data
 IPAddress=`ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{print $1}'`
+process_name=`basename $0`
 
 # Admin email address
-AdminEmail=admin@foo.bar
+AdminEmail=`cat $DataDir/admin_emails|tr '\n' ','`
 
 log()
 {
@@ -16,14 +17,16 @@ log()
 _level=$1
 _msg=$2
 echo "$_level: $(date) | Process: $process_name : $_msg"
-echo "$(date)|$process_name|$_level|$_msg" >>$LogDir/purge_log.log
+echo "$(date)|$process_name|$_level|$_msg" >>$LogDir/$process_name.log
 
 }
 
 for LOG_FILE in $LogDir/*.log
 do
-	/bin/grep "ERROR" $LOG_FILE > $DataDir/mailbody
-	cat $DataDir/mailbody|/bin/mail -s "project mtss error report. $LOG_FILE on $IPAddress" $AdminEmail 2>&1 &&  log INFO "purge_log.sh stopped" || log ERROR "purge_log.sh quit unexpectly"	 
-	>$LOG_FILE
+        /bin/grep "ERROR" $LOG_FILE > $DataDir/mailbody
+        if [[ -s $DataDir/mailbody ]]
+        then
+                cat $DataDir/mailbody|/bin/mail -s "project mtss error report. $LOG_FILE on $IPAddress" $AdminEmail 2>&1 &&  log INFO "$process_name stopped" || log ERROR "$process_name quit unexpectly"   
+        fi
+        >$LOG_FILE
 done
-
